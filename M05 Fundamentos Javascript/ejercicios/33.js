1784cf59cc09bd1863b59ff53df8ba6a3ecfd45b
Gamma <santiagogamam@outlook.com>
2024-01-04 21:03:50 -0500

    Modulo 05

--- M05 Fundamentos Javascript/ejercicios/33.js
+++ M05 Fundamentos Javascript/ejercicios/33.js
@@ -6,6 +6,17 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  var primo = 0
+  for (var i = 1; i <= numero; i++) {
+    if (numero % i === 0) {
+      primo++;
+    }
+  }
+  if (primo <= 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = esNumeroPrimo;
