695abbb7a927c9689805bd69afe40584cf84996f
Gamma <santiagogamam@outlook.com>
2024-01-05 21:27:32 -0500

    Avance mudulo 07 y correccion test M7T5

--- M07 Arrays/ejercicios/05.js
+++ M07 Arrays/ejercicios/05.js
@@ -1,6 +1,8 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
+  var stringLargo = array.find( (string) => string.length >= 5);
+  return stringLargo;
 }
 
 module.exports = obtenerPrimerStringLargo;
