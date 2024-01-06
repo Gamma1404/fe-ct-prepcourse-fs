5e265de5569dcd67146be0616a374d19e207cf60
Gamma <santiagogamam@outlook.com>
2024-01-05 21:18:02 -0500

    Avance modulo 6

--- M06 Bucles/ejercicios/22.js
+++ M06 Bucles/ejercicios/22.js
@@ -3,6 +3,36 @@ function diasEnMes(mes) {
   // Determine cúantos días tiene el mes correspondiente a ese número.
   // PISTA: Usa un bucle do-while para validar que el mes ingresado sea válido.
   // Tu código:
+  var diasEnMes = 0;
+
+  switch (mes) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      diasEnMes = 31;
+      break;
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      diasEnMes = 30;
+      break;
+
+    case 2:
+      diasEnMes = 28;
+      break;
+
+    default:
+      diasEnMes = 0;
+      break;
+  }
+
+  return diasEnMes;
 }
 
 module.exports = diasEnMes;
--- M06 Bucles/ejercicios/24.js
+++ M06 Bucles/ejercicios/24.js
@@ -2,6 +2,14 @@ function invertirTexto(texto) {
   // La función recibe un argumento "texto" que es un string.
   // Tu objetivo es invertir el string y devolver el string invertido.
   // Tu código:
+  var stringInvertido = [];
+  var stringSeparado = texto.split('');
+
+  for (var i = 0; i < stringSeparado.length; i++) {
+    stringInvertido.unshift(stringSeparado[i]);    
+  }
+
+  return texto = stringInvertido.join('');
 }
 
 module.exports = invertirTexto;
